aac1dea3b94657755977dbbc1d4ef49ea5473240
George <george@Georges-MacBook-Pro.local>
2016-01-28 15:23:23 +1100

    implemented bitflip on bitarray

--- sieve.c
+++ sieve.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "sieve.h"
+
 #define MAXINT 128
 
 unsigned int bitfield[MAXINT/sizeof(unsigned int)/8] = {0};
-void biton(int in, unsigned out[]);
+
 
 
 int main(int argc, char* argv[]) {
@@ -12,17 +14,46 @@ int main(int argc, char* argv[]) {
 
 	}
 */
-	biton(1,bitfield);
+	bitflip(2,bitfield);
 	printf("Bitfield[0]: %d\n",bitfield[0]);
 
-	biton(3, bitfield);
+	bitflip(4, bitfield);
 	printf("Bitfield[0]: %d\n",bitfield[0]);
 
+	bitflip(2, bitfield);
+	printf("Bitfield[0]: %d\n",bitfield[0]);	
+
+	bitflip(4, bitfield);
+	printf("Bitfield[0]: %d\n",bitfield[0]);
+}
+
+/* bitmasking logic 
+	
+	int | 1<<(loc) = set bit at loc to 1
+	0000 0000 | 0000 0010 = 0000 0010
+	
+	int & 1<<(loc) = set bit at loc to 0
+	0000 1010 & 0000 0010 = 0000 1000
+
+*/
+
+void bitflip(int in, unsigned out[]) {
+	// can potentially optimise modulo to rounding
+	int mod = in%8;
+	int bitindex = (in - mod)/8;
+	out[bitindex] = out[bitindex] ^ 0x1<<(mod-1);
 }
 
 void biton(int in, unsigned out[]) {
-	// can potentially optimise modulo to round
+	// can potentially optimise modulo to rounding
 	int mod = in%8;
 	int bitindex = (in - mod)/8;
-	out[bitindex] = 1<<(mod-1);
+	out[bitindex] = out[bitindex] | 0x1<<(mod-1);
+}
+
+void bitoff(int in, unsigned out[]) {
+	// can potentially optimise modulo to rounding
+	int mod = in%8;
+	int bitindex = (in - mod)/8;
+	out[bitindex] = out[bitindex] & 0x1<<(mod-1);
 }
---
+++ sieve.h
@@ -0,0 +1,7 @@
+#pragma once
+
+void bitflip(int in, unsigned out[]);
+
+void biton(int in, unsigned out[]);
+
+void bitoff(int in, unsigned out[]);
