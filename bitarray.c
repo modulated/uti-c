8eb9bbf4cc4c5f8bfbc53016e93e098b3348eb47
George <george@ua-unistudent-ten-9-225-23.uniaccess.unimelb.edu.au>
2016-02-02 14:06:58 +1100

    bitarray module

---
+++ bitarray.c
@@ -0,0 +1,32 @@
+
+
+void bitflip(int in, unsigned out[]) {
+	// can potentially optimise modulo to rounding
+	int mod = in%8;
+	int bitindex = (in - mod)/8;
+	out[bitindex] = out[bitindex] ^ 0x1<<(mod-1);
+}
+
+int bitread(int in, unsigned out[]) {
+	int mod = in%8;
+	int bitindex = (in - mod)/8;	
+	
+	return (out[bitindex] >> (mod-1) ) & 0x1;
+}
+
+/* Need to figure out failsafe way for implementing
+
+void biton(int in, unsigned out[]) {
+	// can potentially optimise modulo to rounding
+	int mod = in%8;
+	int bitindex = (in - mod)/8;
+	out[bitindex] = out[bitindex] | 0x1<<(mod-1);
+}
+
+void bitoff(int in, unsigned out[]) {
+	// can potentially optimise modulo to rounding
+	int mod = in%8;
+	int bitindex = (in - mod)/8;
+	out[bitindex] = out[bitindex] & 0x1<<(mod-1);
+}
+*/
---
+++ bitarray.h
@@ -0,0 +1,9 @@
+#pragma once
+
+void bitflip(int in, unsigned out[]);
+
+int bitread(int in, unsigned out[]);
+
+//void biton(int in, unsigned out[]);
+
+//void bitoff(int in, unsigned out[]);
