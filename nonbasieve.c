3c62130995f9714c17371abab72c68b802f7692c
George <cowkingdom@hotmail.com>
2016-04-01 19:00:59 -0400

    benchmarking ba and array sieves

--- nonbasieve.c
+++ nonbasieve.c
@@ -3,30 +3,37 @@
 #include <math.h>
 #include "bitarray.h"
 
-#define MAXINT (unsigned long)4000000
-#define MAXINTSQR (unsigned long)sqrt(MAXINT)
+//#define MAXINT (unsigned long)4000000
+//#define MAXINTSQR (unsigned long)sqrt(MAXINT)
 
-void flagmult(unsigned long in, struct bitarray* ba) {
+unsigned long MAXINT;
+unsigned long MAXINTSQR;
+
+void flagmult(unsigned long in, unsigned long ba[]) {
 	unsigned long i = in;
 	while( (i = i + in) <= MAXINT) {
-		bitarray_on(ba, (int)i);
+	    ba[i] = 1;
 	}
 }
 
 int main(int argc, char* argv[]) {
-	
-    struct bitarray ba = bitarray_create(MAXINT+1);
+    
+    printf("Enter max number: ");
+    scanf("%lu",&MAXINT);
+    MAXINTSQR = (unsigned long)sqrt(MAXINT);
+
+    unsigned long ba[MAXINT+1];
 	unsigned long i;
 	
 	for (i = 2; i <= MAXINTSQR; i++) {
 		printf("Multing %lu\n",i);
-		flagmult(i, &ba);
+		flagmult(i, ba);
 	}
 	
 	unsigned long count = 0;
 	
 	for (i = 2; i <= MAXINT; i++) {
-		unsigned long out = (unsigned long)bitarray_read(&ba,(int)i);
+		unsigned long out = ba[i];
 		if (out == 0) {
 			printf("Prime: %lu\n", i);
 			count++;
