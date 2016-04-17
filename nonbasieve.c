e43b77b53de85bbdaf78b3306b5cbf394092230a
George <george@Georges-MacBook-Pro.local>
2016-04-17 15:26:54 +1000

    optimized and tweaked output formatting

--- nonbasieve.c
+++ nonbasieve.c
@@ -1,31 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "bitarray.h"
-
-//#define MAXINT (unsigned long)4000000
-//#define MAXINTSQR (unsigned long)sqrt(MAXINT)
 
 unsigned long MAXINT;
 unsigned long MAXINTSQR;
+
+void printfcomma (int n) {
+    if (n < 0) {
+        printf ("-");
+        printfcomma (-n);
+        return;
+    }
+    if (n < 1000) {
+        printf ("%d", n);
+        return;
+    }
+    printfcomma (n/1000);
+    printf (",%03d", n%1000);
+}
 
 void flagmult(unsigned long in, unsigned long ba[]) {
 	unsigned long i = in;
 	while( (i = i + in) <= MAXINT) {
 		
-		if (ba[i] == 0) {
-	    	ba[i] = 1;
-			printf("Flip at %lu, multing %lu\n", i, in);			
-		}
+		ba[i] = 1;
 	}
 }
 
 int main(int argc, char* argv[]) {
-    
-    printf("Enter max number: ");
+	
+    puts("Enter max number: ");
     scanf("%lu",&MAXINT);
     MAXINTSQR = (unsigned long)sqrt(MAXINT);
 
+	// inititalise array of size MAXINT+1.
     unsigned long* ba = calloc(MAXINT+1, sizeof(unsigned long));
 	
 	// Exclude evens, 0 and 1.
@@ -33,34 +41,28 @@ int main(int argc, char* argv[]) {
 	ba[1] = 1;
 	ba[0] = 1;
 	
-	for (unsigned long i = 3; i <= MAXINTSQR; i = i + 2) {
-		printf("Multing %lu\n",i);
-		flagmult(i, ba);
-	}
-	
-	unsigned long count = 1;
-	printf("Prime: 2\n");
+	unsigned long counter = 1;
 	
 	for (unsigned long i = 3; i <= MAXINT; i = i + 2) {
-		unsigned long out = ba[i];
-		if (out == 0) {
-			printf("Prime: %lu\n", i);
-			count++;
+		
+		if (i <= MAXINTSQR) {
+			flagmult(i, ba);
 		}
-	}
+		
+		if (ba[i] == 0) {
+			//printf("Prime: %lu\n", i);
+			counter++;
+		}
+		
+	}	
 	
+	free(ba);
 	
-	printf("Count: %lu\n",count);
+	printfcomma(counter); 
+	printf(" primes under ");
+	printfcomma(MAXINT);
+	puts(".");
 }
 
-/* Pseudo
-
-init bitarray[MAXINT]
-iterate through bitarary elements up to sqrt(MAXINT)
-flag multiples of iteration but not iteration itself
-count
-
-
-*/
 
 
