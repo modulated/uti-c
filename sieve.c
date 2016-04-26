bca40d51212af3be1d882abfa00b736cc9079722
George <george@Georges-MacBook-Pro.local>
2016-04-26 23:49:22 +1000

    timing info added to sieve

--- sieve.c
+++ sieve.c
@@ -1,50 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 #include "bitarray.h"
 
-#define MAXINT (unsigned long)4000000
-#define MAXINTSQR (unsigned long)sqrt(MAXINT)
+unsigned long MAXINT;
+unsigned long MAXINTSQR;
+
+void printfcomma (int n) {
+	if (n < 0) {
+		printf ("-");
+		printfcomma (-n);
+		return;
+	}
+	if (n < 1000) {
+		printf ("%d", n);
+		return;
+	}
+	printfcomma (n/1000);
+	printf (",%03d", n%1000);
+}
 
 void flagmult(unsigned long in, struct bitarray* ba) {
 	unsigned long i = in;
 	while( (i = i + in) <= MAXINT) {
-		bitarray_on(ba, (int)i);
+		
+		bitarray_on(ba, i);
 	}
 }
 
 int main(int argc, char* argv[]) {
 	
-    struct bitarray ba = bitarray_create(MAXINT+1);
-	unsigned long i;
-	
-	for (i = 2; i <= MAXINTSQR; i++) {
-		printf("Multing %lu\n",i);
-		flagmult(i, &ba);
+	if (argc == 1) {
+		puts("Enter max number: ");
+		scanf("%lu",&MAXINT);
 	}
-	
-	unsigned long count = 0;
-	
-	for (i = 2; i <= MAXINT; i++) {
-		unsigned long out = (unsigned long)bitarray_read(&ba,(int)i);
-		if (out == 0) {
-			printf("Prime: %lu\n", i);
-			count++;
+	else {
+		if( (MAXINT = strtoul(argv[1],NULL,10)) == 0) {
+			puts("Error parsing input");
+			exit(1);
 		}
 	}
+		
+	MAXINTSQR = (unsigned long)sqrt(MAXINT);
+
+	if (MAXINT > (int)MAXINT) {printf("Overflow!");}
 	
+	// inititalise array of size MAXINT+1.
+	struct bitarray ba = bitarray_create((int)MAXINT+1);
 	
-	printf("Count: %lu\n",count);
+	// start timing
+	clock_t t1, t2;
+	t1 = clock();
+	
+	// Exclude evens, 0 and 1.
+	flagmult(2, &ba);
+	bitarray_on(&ba, 0);
+	bitarray_on(&ba, 1);
+	
+	unsigned long counter = 1;
+	
+	for (unsigned long i = 3; i <= MAXINT; i = i + 2) {
+		
+		if (i <= MAXINTSQR) {
+			flagmult(i, &ba);
+		}
+		
+		if (bitarray_read(&ba, i) == 0) {
+			//printf("Prime: %lu\n", i);
+			counter++;
+		}
+		
+	}	
+	
+	t2 = clock();
+	float diff = ((float)(t2-t1)/CLOCKS_PER_SEC)*1000;
+	
+	bitarray_destroy(&ba);
+	
+	printfcomma(counter); 
+	printf(" primes under ");
+	printfcomma(MAXINT);
+	printf(" in %.3f milliseconds.\n", diff);
 }
 
-/* Pseudo
-
-init bitarray[MAXINT]
-iterate through bitarary elements up to sqrt(MAXINT)
-flag multiples of iteration but not iteration itself
-count
 
 
-*/
+
 
 
