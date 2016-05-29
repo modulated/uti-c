8f3b2f101496cb389e3949ddd88c070f609bd2e0
George <cowkingdom@hotmail.com>
2016-05-29 15:17:56 +1000

    basic seqc app structure working

--- Makefile
+++ Makefile
@@ -13,7 +13,7 @@ all: $(SRCDIR)/*.c
 	make test
 
 seqc: $(SRCDIR)/seqc.c
-	$(CC) $(CFLAGS) -o bin/seqc src/seqc.c
+	$(CC) $(CFLAGS) -o seqc src/seqc.c src/bioseq.c
 
 test:
 	mkdir -p ./bin
--- src/seqc.c
+++ src/seqc.c
@@ -2,30 +2,85 @@
 #include <stdlib.h>
 #include "bioseq.h"
 #include <getopt.h>
-#include <ctype.h>
+#include <string.h>
 
-void printhelp(void) {
-    puts("seqc bioinformatics tool v0.a\nseqc [-psbrcdo] file");
+// Version
+#define VERSION "0.0.3"
+
+// Flag defines
+#define RNA_F 1<<6
+#define DNA_F 1<<5
+#define PROTEIN_F 1<<4
+#define BUFFER_F 1<<2
+#define OUTFILE_F 1<<3
+#define STDINPUT_F 1
+
+char* outfile = NULL;
+unsigned buffer_size = 0; 
+
+void print_help(void) {
+    puts(        
+        "Usage: seqc [-hsv] [-b buffer size] [-o output] [file]\n"
+		"Arguments:\n"
+		"-h \tShow this message\n"
+		"-s \tRead from standard input\n"
+		"-v \tPrint version info.\n"
+	);
+}
+
+void print_version(void) {
+    puts(
+        "seqc bioinformatics tool "VERSION"\n"
+		"written by modulated"
+     );
+}
+
+void read_stdin() {
+    char in;
+    while ((in = getc(stdin)) != EOF) {
+        putchar(in);
+    }
 }
 
 int parseargs(int argc, char** argv) {
-    int option = 0;
-    while ((option = getopt(argc, argv, "hso:b:")) != -1) {
-        switch (option) {
-            
-            case 'h': printhelp(); break;
-            default: printhelp();
-            exit(1);
+	int option = 0;
+	char arg;
+	
+    while ((arg = getopt(argc, argv, "b:hso:v")) != -1) {
+        switch (arg) {
+			
+			case 'b':
+				buffer_size = atoi(optarg);
+			
+			case 'o': 
+				outfile = strdup(optarg);
+				option = option | OUTFILE_F; 
+				break;
+				
+            case 's': 
+				option = option | STDINPUT_F; 
+				break;
+			
+			case 'v':
+				print_version(); exit(0);
+					
+            case 'h':            
+			default: print_help(); exit(1);
         }
-    }
-
-    return option;
+	}	
+	return option;
 }
 
 int main(int argc, char** argv) {
-    int flags = 0;
-
-    flags = parseargs(argc, argv);
+    
+	int flags;
+	
+    if (argc > 1) {
+		flags = parseargs(argc, argv);
+		printf("Option: %d\n",flags);
+	}
+	
+    else print_help();
     
     return 0;
 }
