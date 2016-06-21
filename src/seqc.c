565c1a9fee8fad8d2a7ef5ec172599b173731c1f
George <cowkingdom@hotmail.com>
2016-06-21 20:33:22 +1000

    started implementing diffing in seqc

--- src/seqc.c
+++ src/seqc.c
@@ -8,11 +8,11 @@
 #include "color.h"
 
 // Version
-#define VERSION "0.1.4"
+#define VERSION "0.1.6"
 
 // Flag defines
 #define ELECTRO_F 0x1<<6
-#define RNA_F 0x1<<5
+#define DIFF_F 0x1<<5
 #define READ_F 0x1<<4
 #define STATS_F 0x1<<3
 #define OUTFILE_F 0x1<<2
@@ -21,15 +21,18 @@
 // Global vars
 static int flags;
 static FILE* infilepointer = NULL;
+static FILE* infilepointer2 = NULL;
 static FILE* outfilepointer = NULL;
 static char* infilestring = NULL;
+static char* infilestring2 = NULL;
 static char* outfilestring = NULL;
 static bioseq_stats stats;
 
 void print_help(void) {
     puts(        
-        "Usage: seqc [-hvp] [-b buffer size (default 1000)] [-o output] [input file]\n"
+        "Usage: seqc [-dhvp] [-o output] file1 ...\n"
 		"Arguments:\n"
+		"-d \tDiff DNA sequences.\n"
 		"-h \tShow this message\n"
 		"-v \tPrint version info.\n"
 		"-p \tConvert to protein.\n"
@@ -58,10 +61,31 @@ void print_stats(void) {
 * File Handling Functions
 */
 
+size_t file_size(FILE* file) {
+	
+	fseek(file, 0L, SEEK_END);
+	size_t size = ftell(file);
+	rewind(file);
+
+	return size;
+}
+
 void open_infile() {
     infilepointer = fopen(infilestring, "r");	
 	
 	if (infilepointer == NULL) {
+		puts("Error: cannot access input file.");
+		printf("%s\n",strerror(errno));
+		exit(errno);
+	}
+	
+	flockfile(infilepointer);
+}
+
+void open_infile2() {
+    infilepointer2 = fopen(infilestring2, "r");	
+	
+	if (infilepointer2 == NULL) {
 		puts("Error: cannot access input file.");
 		printf("%s\n",strerror(errno));
 		exit(errno);
@@ -104,9 +128,9 @@ char read_stdin() {
 		return in;
 	}
 	
-	else {		
+	else {
 		return read_stdin();
-	}		
+	}
 }
 
 char read_infile() {
@@ -135,10 +159,10 @@ void write_outfile(char out) {
 }
 
 /*
-** Loop Functions
+** Protein Loop Functions
 */
 
-void stdin_stdout_loop() {
+void stdin_stdout_protein() {
 	char in[3] = {'\0','\0','\0'};
 	int cont = 1;
 	
@@ -158,7 +182,7 @@ void stdin_stdout_loop() {
 	putchar('\n');
 }
 
-void filein_stdout_loop() {
+void filein_stdout_protein() {
 	char in[3] = {'\0','\0','\0'};
 	int cont = 1;
 	open_infile();
@@ -178,7 +202,7 @@ void filein_stdout_loop() {
 	close_infile();
 }
 
-void filein_stats_loop() {
+void filein_stats_protein() {
 	char in[3] = {'\0','\0','\0'};
 	int cont = 1;
 	open_infile();
@@ -197,7 +221,7 @@ void filein_stats_loop() {
 	close_infile();
 }
 
-void stdin_stats_loop() {
+void stdin_stats_protein() {
 	char in[3] = {'\0','\0','\0'};
 	int cont = 1;
 	
@@ -213,7 +237,7 @@ void stdin_stats_loop() {
 	puts("");
 }
 
-void filein_fileout_loop() {
+void filein_fileout_protein() {
 	char in[3] = {'\0','\0','\0'};
 	int cont = 1;
 	open_infile();
@@ -235,7 +259,7 @@ void filein_fileout_loop() {
 	close_outfile();
 }
 
-void stdin_fileout_loop() {
+void stdin_fileout_protein() {
 	char in[3] = {'\0','\0','\0'};
 	int cont = 1;	
 	open_outfile();
@@ -255,11 +279,49 @@ void stdin_fileout_loop() {
 	close_outfile();
 }
 
+void fileout_diff() {
+	
+}
+
+void stdout_diff() {
+	
+	open_infile();
+	open_infile2();
+
+	size_t insize = file_size(infilepointer);
+	size_t insize2 = file_size(infilepointer2);
+
+	printf("%s: %lu \t %s: %lu\n", infilestring, insize, infilestring2, insize2);
+
+	char* bufferfile1 = malloc((insize + 1) * sizeof(char));
+	char* bufferfile2 = malloc((insize2 + 1) * sizeof(char));
+
+	fread (bufferfile1, 1, insize, infilepointer);
+	fread (bufferfile2, 1, insize2, infilepointer2);
+
+	int similarity = bioseq_string_similarity(bufferfile1, bufferfile2);
+	int insertions = bioseq_string_insertions(bufferfile1, bufferfile2);
+	int deletions = bioseq_string_deletions(bufferfile1, bufferfile2);
+
+	printf("Similarity: %d\n", similarity);
+	printf("Insertions: %d\n", insertions);
+	printf("Deletions: %d\n", deletions);
+}
+
+void seq_diff() {
+	if (infilestring == NULL || infilestring2 == NULL) {
+		puts("Please supply two files.");
+		exit(1);
+	}
+	if (outfilestring != NULL) fileout_diff();
+	if (outfilestring == NULL) stdout_diff();
+}
+
 void seq_protein() {
-	if (infilestring == NULL && outfilestring == NULL) stdin_stdout_loop();
-	if (infilestring != NULL && outfilestring == NULL) filein_stdout_loop();
-	if (infilestring != NULL && outfilestring != NULL) filein_fileout_loop();
-	if (infilestring == NULL && outfilestring != NULL) stdin_fileout_loop();
+	if (infilestring == NULL && outfilestring == NULL) stdin_stdout_protein();
+	if (infilestring != NULL && outfilestring == NULL) filein_stdout_protein();
+	if (infilestring != NULL && outfilestring != NULL) filein_fileout_protein();
+	if (infilestring == NULL && outfilestring != NULL) stdin_fileout_protein();
 	
 	flags = flags | READ_F;
 }
@@ -267,8 +329,8 @@ void seq_protein() {
 void seq_stats() {
 		
 		if (!(flags & READ_F)) {
-			if (infilestring == NULL) stdin_stats_loop();
-			else filein_stats_loop();
+			if (infilestring == NULL) stdin_stats_protein();
+			else filein_stats_protein();
 		}
 		
 		bioseq_stats_calculate(&stats);
@@ -283,6 +345,10 @@ void seq_determine() {
 	if (flags & STATS_F) {
 		seq_stats();
 	}
+
+	if (flags & DIFF_F) {
+		seq_diff();
+	}
 }
 
 // Parse args and set flags and values
@@ -290,19 +356,23 @@ int parseargs(int argc, char** argv) {
 	int option = 0;
 	char arg;
 	
-    while ((arg = getopt(argc, argv, "ho:psv")) != -1) {
+    while ((arg = getopt(argc, argv, "dho:psv")) != -1) {
         switch (arg) {
 			
+			case 'd': 
+				option |= DIFF_F;
+				break;
+
 			case 'o': 
 				outfilestring = strdup(optarg);
 				break;
 				
 			case 'p':
-				option = option | PROTEIN_F;
+				option |= PROTEIN_F;
 				break;
 				
 			case 's':
-				option = option | STATS_F;
+				option |= STATS_F;
 				break;
 				
 			case 'v':
@@ -315,12 +385,27 @@ int parseargs(int argc, char** argv) {
 	
     if (argv[optind] != NULL) { // If arg after last option exists
 		
-        if (optind + 1 == argc) { // One additional argument after options
-			infilestring = argv[optind];
+		// If -p
+		if (option & PROTEIN_F) {
+			if (optind + 1 == argc) { // One additional argument after options
+				infilestring = argv[optind];
+			}
+			else {
+				puts("Error: too many input files. Please supply one.");
+				exit(1);
+			}
 		}
-		else {
-			puts("Error: too many input files. Please supply one.");			
-			exit(1);
+
+		// If -d
+		if (option & DIFF_F) {
+			if (optind + 2 == argc) { // Two additional argument after options
+				infilestring = argv[optind];
+				infilestring2 = argv[optind+1];
+			}
+			else {
+				puts("Error: Please supply two input files.");
+				exit(1);
+			}
 		}
     }
     	
