68b9702e28574d766003ec6925f246dcfe86452e
George <cowkingdom@hotmail.com>
2016-08-13 00:11:56 +1000

    working hashmap implementation

---
+++ include/hashmap.h
@@ -0,0 +1,47 @@
+#ifndef _HASHMAP_H
+#define _HASHMAP_H
+
+// ERROR values
+#define HASHMAP_MISSING -3  /* No such element */
+#define HASHMAP_FULL    -2 	/* Hashmap is full */
+#define HASHMAP_OMEM    -1  /* Out of Memory */
+#define HASHMAP_OK       0  /* OK */
+
+/* Pointer to arbitrary data */
+typedef void* any_t; 
+
+/* Pointer to function that can take two arguments
+ * and return an int as a status code.
+ */
+typedef int (*FPany)(any_t, any_t); 
+
+/* Pointer to abstracted map structure. */
+typedef any_t map_t;
+
+/* Return empty hashmap. Returns NULL if empty. */
+extern map_t hashmap_new();
+
+/* Iterativly call *fp* with args (item, data) for each
+ * element *data* in hashmap. Function must return status 
+ * code. Anything other than MAP_OK terminates traversal.
+ */
+extern int hashmap_itr(map_t this, FPany fp, any_t item);
+
+/* Add element to hashmap. Returns MAP_OK or MAP_OMEM. */
+extern int hashmap_add(map_t this, char* key, any_t value);
+
+/* Gets element from hashmap. Returns MAP_OK or MAP_MISSING. */
+extern int hashmap_get(map_t this, char* key, any_t* arg);
+
+/* Delete element from hashmap. Returns MAP_OK or MAP_MISSING. */
+extern int hashmap_del(map_t this, char* key);
+
+extern int hashmap_len(map_t this);
+
+/* Frees memory associated with hashmap. */
+extern void hashmap_free(map_t this);
+
+/** Fills buffer with error string. Buffer should be >16 bytes. */
+extern void hashmap_error(int error, char* buffer, unsigned long buffer_len);
+
+#endif
---
+++ tests/hashmap.test.c
@@ -0,0 +1,147 @@
+#include "tap.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "../include/hashmap.h"
+
+
+void test_hashmap_new (void)
+{
+	map_t map = hashmap_new();
+	ok(map != NULL, "hashmap_new.");
+	
+	hashmap_free(map);
+}
+
+void test_hashmap_free (void)
+{	
+	map_t map = hashmap_new();
+	hashmap_free(map);
+
+	skip(map == NULL, "hashmap_free.") ||
+	diag("Expected: %p got: %p", NULL, map);
+}
+
+void test_hashmap_add (void)
+{
+	map_t map = hashmap_new();
+	const char key[] = "test";
+	const int val = 24;
+	int error = hashmap_add(map, (char*)key, (void*)&val);
+	ok(error == HASHMAP_OK, "hashmap_add.") || diag("Expected: %d got: %d", HASHMAP_OK, error);
+
+	hashmap_free(map);
+}
+
+void test_hashmap_get (void)
+{
+	map_t map = hashmap_new();
+	const char key[] = "No sleep";
+	const int val = 24;
+	any_t set_box = (any_t)&val;
+	int error = hashmap_add(map, (char*)key, set_box);
+	if (error != HASHMAP_OK)
+	{
+		printf("Error adding to hashmap: %d\n", error);
+		exit(error);
+	}
+	
+	any_t ptr;
+	error = hashmap_get(map, (char*)key, &ptr);
+	int get_val = *(int*)ptr;
+	
+	if (error != HASHMAP_OK)
+	{
+		char error_str[32];
+		hashmap_error(error, error_str, 32);
+		printf("ERROR getting from hashmap: %s\n", error_str);
+		exit(error);
+	}
+
+	ok(get_val == 24, "hashmap_get.") || diag("Expected: %d got: %d", val, get_val);
+
+	hashmap_free(map);
+}
+
+void test_hashmap_del()
+{
+	map_t map = hashmap_new();
+	const char key[] = "No sleep";
+	const int val = 24;
+	any_t set = (any_t)&val;
+	int error = hashmap_add(map, (char*)key, set);
+	if (error != HASHMAP_OK)
+	{
+		char error_str[32];
+		hashmap_error(error, error_str, 32);
+		printf("ERROR adding to hashmap: %s\n", error_str);
+		exit(error);
+	}
+	
+	any_t ptr;
+	error = hashmap_get(map, (char*)key, &ptr);
+	
+	if (error != HASHMAP_OK)
+	{
+		char error_str[32];
+		hashmap_error(error, error_str, 32);
+		printf("ERROR getting from hashmap: %s\n", error_str);
+		exit(error);
+	}
+
+	int del_error = hashmap_del(map, (char*)key);
+
+	int get_error = hashmap_get(map, (char*)key, &ptr);
+
+	ok(
+		del_error == HASHMAP_OK &&
+		get_error == HASHMAP_MISSING,
+		"hashmap_del.") ||
+	diag("Del: %d Get: %d\n", del_error, get_error);
+
+	hashmap_free(map);
+}
+
+void test_hashmap_len()
+{
+	map_t map = hashmap_new();
+	const int exp_init = 0;
+	int len_init = hashmap_len(map);
+
+	const char key[] = "key";	
+	hashmap_add(map, (char*)key, (any_t)&key);
+	const int exp_add = 1;
+	int len_add = hashmap_len(map);
+
+	hashmap_add(map, (char*)key, (any_t)&key);
+	int len_readd = hashmap_len(map);
+
+	hashmap_del(map, (char*)key);
+	int len_del = hashmap_len(map);
+
+	ok(
+		len_init == exp_init &&
+		len_add == exp_add &&
+		len_readd == exp_add &&
+		len_del == exp_init,
+		"hashmap_len."
+	);
+
+	hashmap_free(map);
+}
+
+void test_all (void)
+{
+	test_hashmap_new();
+	test_hashmap_free();
+	test_hashmap_add();
+	test_hashmap_get();
+	test_hashmap_del();
+	test_hashmap_len();
+}
+
+int main ()
+{
+	plan_no_plan();
+	test_all();
+	return exit_status();
+}
